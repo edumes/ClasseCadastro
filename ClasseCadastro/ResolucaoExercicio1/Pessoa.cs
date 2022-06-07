f1d7e3a2c8b5c0048284890e289d87100e31fb33
edumes <66120423+edumes@users.noreply.github.com>
2022-06-07 20:48:15 -0300

    Add files via upload

---
+++ ClasseCadastro/ResolucaoExercicio1/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroCarneiro
+{
+    /*
+     Conceitos de Orientação a Objetos usados
+     Classes, Objeto, Abstração, Encapsulamento
+     */
+    public class Carneiro
+    {
+        public string numero { get; set; }
+        public string numero_mae { get; set; }
+        public string nome_pai { get; set; }
+        public string sexo { get; set; }
+        public string data_nascimento { get; set; }
+
+    }
+}
---
+++ ClasseCadastro/ResolucaoExercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace CadastroCarneiro
+{
+    /*
+     Conceitos de Orientação a Objetos usados
+     Classes, Instância, Método
+     */
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Carneiro carneiro = new Carneiro();
+            Console.WriteLine("Preencha o Numero do Animal:");
+            carneiro.numero = Console.ReadLine();
+            Console.WriteLine("Digite o Numero da Mãe:");
+            carneiro.numero_mae = Console.ReadLine();
+            Console.WriteLine("Digite o Nome do Pai:");
+            carneiro.nome_pai = Console.ReadLine();
+            //pessoa.numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Preencha o Sexo/Genero:");
+            carneiro.sexo = Console.ReadLine();
+            Console.WriteLine("Preencha a Data de Nascimento:");
+            carneiro.data_nascimento = Console.ReadLine();
+            MostrarDadosCarneiro(carneiro);
+        }
+
+        static void MostrarDadosCarneiro(Carneiro carneiro)
+        {
+            Console.WriteLine("\n Número: {0}\n Número da Mãe: {1}\n Nome do Pai: {2}\n Sexo: {3}\n Data de Nascimento: {4}",
+              carneiro.numero, carneiro.numero_mae, carneiro.nome_pai, carneiro.sexo, carneiro.data_nascimento);
+            Console.ReadKey();
+        }
+    }
+}
